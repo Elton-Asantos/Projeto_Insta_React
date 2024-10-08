68543626cbb0e44128b4731462eb68bed9de482b
Elton_Dev <168014499+Elton-Asantos@users.noreply.github.com>
2024-10-08 17:46:38 -0300

    Add files via upload

---
+++ src/componentes/Post.js
@@ -0,0 +1,12 @@
+export default function Post() {
+  return (
+    <div className="Post">
+      <div className=" Post-cabecalho"> </div>
+      <div className="Post-avatar"> </div>
+      <div className="Post-nome">  </div>
+      <div className="Post-conteudo">
+        <img  src="https://picsum.photos/200/300" /></div>
+
+    </div>
+  );
+}
